839c56fc3570d42a125f4c98279a9756552bafbe
nesu <soccernesu@yahoo.com>
2021-12-16 01:32:33 -0500

    nft asset crud added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import React, { Component } from 'react'
-// import { Segment } from 'semantic-ui-react'
+import { Table, Label, Form, Input, Button } from 'semantic-ui-react'
 import HeaderMain from './components/Header'
-// import NewForm from './components/NewForm'
+import NewForm from './components/NewForm'
+// import AssetList from './components/AssetList'
 import Footer from './components/Footer'
 import HomePage from './components/HomePage'
 import Featured from './components/Featured'
@@ -18,50 +19,61 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      baseURL: 'https://flow-mainnet.g.alchemy.com',
-      apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
-      query: '&t=',
-      assetTitle: '',
-      searchURL: '',
-      assetsForSale: [],
-      requestOptions: {}
-    }
     // this.state = {
-    //   assets: [],
-    //   modalOpen: false,
-    //   asssetToBeEdited: {},
-    //   name:'',
-    //   creator:'',
-    //   price:''
+    //   baseURL: 'https://flow-mainnet.g.alchemy.com',
+    //   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+    //   query: '&t=',
+    //   assetTitle: '',
+    //   searchURL: '',
+    //   assetsForSale: [],
+    //   requestOptions: {}
     // }
+    this.state = {
+      assets: [],
+      modalOpen: false,
+      asssetToBeEdited: {},
+      name:'',
+      creator:'',
+      price:'',
+      userLoggedIn: false
+    }
   }
   
-  getAssets = () =>{
-
-    this.setState({
-      requestOptions: {
-        method: 'GET',
-        redirect: 'follow'
-      }
-    }, () => {
-      fetch("https://flow-mainnet.g.alchemy.com/" +this.state.apiKey+ "/v1/getNFTs/?owner=0x9eef2e4511390ce4&offset=0&limit=10", this.state.requestOptions)
-      .then(res => {
-        return res.json()
-      })
-      .then(json => this.setState({
-        assetsForSale: json
-      }),
-      (err) => console.log(err))
+  getAssets = () => {
+    // fetch to the backend
+    fetch(baseUrl + "/assets",{
+      credentials: "include"
     })
-  // .then(response => response.text())
-  // .then(result => this.setState({
-  //   gamesOnSale: result
-  // }))
-  // .then(console.log(this.state.gamesOnSale.map(function(item){return item})))
-  // .catch(error => console.log('error', error));
-
+    .then(res => {
+      if(res.status === 200) {
+        return res.json()
+      } else {
+        return []
+      }
+    }).then(data => {
+      console.log(data)
+      this.setState({ assets: data })
+    })
   }
+
+  // getAssets = () =>{
+
+  //   this.setState({
+  //     requestOptions: {
+  //       method: 'GET',
+  //       redirect: 'follow'
+  //     }
+  //   }, () => {
+  //     fetch("https://flow-mainnet.g.alchemy.com/" +this.state.apiKey+ "/v1/getNFTs/?owner=0x9eef2e4511390ce4&offset=0&limit=10", this.state.requestOptions)
+  //     .then(res => {
+  //       return res.json()
+  //     })
+  //     .then(json => this.setState({
+  //       assetsForSale: json
+  //     }),
+  //     (err) => console.log(err))
+  //   }) 
+  // }
 
   loginUser = async (e) => {
     console.log('loginUser')
@@ -137,95 +149,92 @@ class App extends Component {
     })
   }
 
-    getData = (data) => {
-      console.log('setting state')
+    // getData = (data) => {
+    //   console.log('setting state')
+    //   this.setState({
+    //     showThisGame: data,
+    //   }, () => {
+    //     console.log(this.state.showThisAsset)
+    //     return this.state.showThisAsset
+    //   })
+    // }
+
+  addAsset = (newAsset) => {
+    const copyAssets = [...this.state.assets]
+    copyAssets.push(newAsset)
+    this.setState({
+      assets: copyAssets,
+    })
+  }
+
+  deleteAsset = (id) => {
+      // console.log(id)
+      fetch(baseUrl + '/assets/' + id, {
+      method: 'DELETE',
+      credentials: "include"
+    }).then( res => {
+      console.log(res)
+      const findIndex = this.state.assets.findIndex(asset => asset._id === id)
+      const copyAssets = [...this.state.assets]
+      copyAssets.splice(findIndex, 1)
       this.setState({
-        showThisGame: data,
-      }, () => {
-        console.log(this.state.showThisAsset)
-        /*made it return to try and pass it to GameInfo*/
-        return this.state.showThisAsset
+        assets: copyAssets
       })
+    })
+  }
+
+  addLike = (asset) => {
+    fetch(baseUrl + '/assets/' + asset._id, {
+      method: 'PUT',
+      body: JSON.stringify({ likes: asset.likes + 1}),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: "include"
+    }).then(res => res.json())
+    .then(resJson => {
+      // console.log(resJson)
+      const copyAssets = [...this.state.assets]
+      const findIndex = this.state.assets.findIndex(asset => asset._id === resJson._id)
+      copyAssets[findIndex].likes = resJson.likes
+      this.setState({
+        assets: copyAssets
+      })
+    })
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault()
+    const url = baseUrl + '/assets/' + this.state.assetToBeEdited._id
+    try{
+      const response = await fetch( url , {
+        method: 'PUT',
+        body: JSON.stringify({
+          name: e.target.name.value,
+          creator: e.target.creator.value
+        }),
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+        credentials: "include"
+      })
+
+      if (response.status === 200){
+        const updatedAsset = await response.json()
+        console.log(updatedAsset)
+        const findIndex = this.state.assets.findIndex(asset => asset._id === updatedAsset._id)
+        const copyAssets = [...this.state.assets]
+        copyAssets[findIndex] = updatedAsset
+        this.setState({
+          assets: copyAssets,
+          modalOpen:false
+        })
+      }
     }
-
-  // addAsset = (newAsset) => {
-  //   const copyAssets = [...this.state.assets]
-  //   copyAssets.push(newAsset)
-  //   this.setState({
-  //     assets: copyAssets,
-  //   })
-  // }
-
-  // deleteAsset = (id) => {
-  //     // console.log(id)
-  //     fetch(baseUrl + '/assets/' + id, {
-  //     method: 'DELETE',
-  //     credentials: "include"
-  //   }).then( res => {
-  //     // console.log(res)
-  //     // if I checked for a 200 response code
-  //     const findIndex = this.state.assets.findIndex(asset => asset._id === id)
-  //     const copyAssets = [...this.state.assets]
-  //     copyAssets.splice(findIndex, 1)
-  //     this.setState({
-  //       assets: copyAssets
-  //     })
-  //   })
-  // }
-
-  // addLike = (asset) => {
-  //   // console.log(holiday)
-  //   fetch(baseUrl + '/assets/' + asset._id, {
-  //     method: 'PUT',
-  //     body: JSON.stringify({ likes: asset.likes + 1}),
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     credentials: "include"
-  //   }).then(res => res.json())
-  //   .then(resJson => {
-  //     // console.log(resJson)
-  //     const copyAssets = [...this.state.assets]
-  //     const findIndex = this.state.assets.findIndex(asset => asset._id === resJson._id)
-  //     copyAssets[findIndex].likes = resJson.likes
-  //     this.setState({
-  //       assets: copyAssets
-  //     })
-  //   })
-  // }
-
-  // handleSubmit = async (e) => {
-  //   e.preventDefault()
-  //   const url = baseUrl + '/assets/' + this.state.assetToBeEdited._id
-  //   try{
-  //     const response = await fetch( url , {
-  //       method: 'PUT',
-  //       body: JSON.stringify({
-  //         name: e.target.name.value,
-  //         creator: e.target.creator.value
-  //       }),
-  //       headers: {
-  //         'Content-Type' : 'application/json'
-  //       },
-  //       credentials: "include"
-  //     })
-
-  //     if (response.status === 200){
-  //       const updatedAsset = await response.json()
-  //       //console.log(updatedHoliday)
-  //       const findIndex = this.state.assets.findIndex(asset => asset._id === updatedAsset._id)
-  //       const copyAssets = [...this.state.assets]
-  //       copyAssets[findIndex] = updatedAsset
-  //       this.setState({
-  //         assets: copyAssets,
-  //         modalOpen:false
-  //       })
-  //     }
-  //   }
-  //   catch(err){
-  //     console.log('Error => ', err);
-  //   }
-  // }
+    catch(err){
+      console.log('Error => ', err);
+    }
+  }
 
   handleChange = (e)=>{
    this.setState({
@@ -252,41 +261,45 @@ class App extends Component {
     return (
       <>  
       <div className='mainContainer'>
-      {/*<NewForm baseUrl={baseUrl} addAsset={ this.addAsset }/>
-        <table>
-          <tbody>
-            { this.state.assets.map((asset, i) => {
+      <NewForm baseUrl={baseUrl} addAsset={ this.addAsset }/>
+      <div className='ui container'>
+      <Table>
+          <Table.Body>
+            { 
+              this.state.assets.map((asset, i) => {
                 return (
-                  <tr key={asset._id}>
-                    <td key={i}> {asset.name} </td>
-                    <td>{asset.likes}</td>
-                    <td onClick={() => { this.addLike(asset)}}>LIKE</td>
-                    <td onClick={() => { this.showEditForm(asset)}}>Show Edit Form</td>
-                    <td onClick={() => this.deleteAsset(asset._id)}>X</td>
-                  </tr>
+                  <Table.Row key={asset._id}>
+                    <Table.Cell key={i}> {asset.name} </Table.Cell>
+                    <Table.Cell>{asset.likes}</Table.Cell>
+                    <Table.Cell onClick={() => { this.addLike(asset)}}>LIKE</Table.Cell>
+                    <Table.Cell onClick={() => { this.showEditForm(asset)}}>Show Edit Form</Table.Cell>
+                    <Table.Cell onClick={() => this.deleteAsset(asset._id)}>X</Table.Cell>
+                  </Table.Row>
                 )
               })
             }
-          </tbody>
-        </table>
+          </Table.Body>
+        </Table>
         <br/>
         {
             this.state.modalOpen &&
 
-            <form onSubmit={this.handleSubmit}>
-              <label>Name: </label>
-              <input name="name" value={this.state.name} onChange={this.handleChange}/> <br/>
+            <Form onSubmit={this.handleSubmit}>
+              <Label>Name: </Label>
+              <Input name="name" value={this.state.name} onChange={this.handleChange}/> <br/>
 
-              <label>Creator: </label>
-              <input name="creator" value={this.state.creator} onChange={this.handleChange}/>
+              <Label>Creator: </Label>
+              <Input name="creator" value={this.state.creator} onChange={this.handleChange}/>
 
-              <label>Price: </label>
-              <input name="price" value={this.state.price} onChange={this.handleChange}/>
+              <Label>Price: </Label>
+              <Input name="price" value={this.state.price} onChange={this.handleChange}/>
 
-              <button>submit</button>
+              <Button>Submit</Button>
 
-            </form>
-          }*/}
+            </Form>
+          }
+      </div>
+      {/*<AssetList />*/}
         <Router>
         <div className='ui segment main' style={{ margin: '0em 0em 0em', padding: '0em 0em 0em' }}>
         <HeaderMain />
--- src/components/AssetList.js
+++ src/components/AssetList.js
@@ -1,80 +1,47 @@
 import React, { Component } from 'react'
-import { Table, Label, Header } from 'semantic-ui-react'
+import { Table, Label, Form, Input, Button } from 'semantic-ui-react'
 
-class AssetList extends Component {
-
-  constructor(props){
-    super(props)
-
-    this.state = {
-      showThisAssetID: '',
-    }
-    this.showSearchedAsset = this.showSearchedAsset.bind(this)
-  }
-
-  showSearchedGame = (event) => {
-    this.setState({
-      requestOptions: {
-        method: 'GET',
-        redirect: 'follow'
-      },
-
-    }, () => {
-      fetch("https://flow-mainnet.g.alchemy.com/xlkp1n3yzg6xim4mwjr2byhpr0wirck7/v1/getNFTMetadata/?tokenId=" + this.state.showThisAssetID)
-      .then(res => {
-        return res.json()
-      })
-      .then(json => this.setState({
-        showThisAsset: json
-      }),
-    (err) => console.log(err))
-    .then(this.props.sendData(this.state.showThisAsset))
-    })
-  }
+export default class AssetList extends Component {
 
   render(){
     return(
       <div className='ui container'>
-      <br/>
-      <Header size='huge' align='center'>Available NFTs</Header>
-      <Table color='blue' celled striped inverted>
-      <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>Game</Table.HeaderCell>
-        <Table.HeaderCell>Price</Table.HeaderCell>
-        <Table.HeaderCell>Rating</Table.HeaderCell>
-      </Table.Row>
-      </Table.Header>
-      <Table.Body>
+      <Table>
+          <Table.Body>
+            { 
+              this.props.assets.map((asset, i) => {
+                return (
+                  <Table.Row key={asset._id}>
+                    <Table.Cell key={i}> {asset.name} </Table.Cell>
+                    <Table.Cell>{asset.likes}</Table.Cell>
+                    <Table.Cell onClick={() => { this.addLike(asset)}}>LIKE</Table.Cell>
+                    <Table.Cell onClick={() => { this.showEditForm(asset)}}>Show Edit Form</Table.Cell>
+                    <Table.Cell onClick={() => this.deleteAsset(asset._id)}>X</Table.Cell>
+                  </Table.Row>
+                )
+              })
+            }
+          </Table.Body>
+        </Table>
+        <br/>
         {
-          this.props.gamesOnSale.map(game => {
-            return(
-              <Table.Row key={game.gameID} onClick={(e) => this.setState({showThisGameID: game.gameID})}>
-                <Table.Cell>
-                  <Label onClick={this.showSearchedGame}>{game.title}</Label>
-                </Table.Cell>
-                <Table.Cell>
-                  <Label>${game.salePrice}</Label>
-                </Table.Cell>
-                <Table.Cell>
-                  <Label>{game.steamRatingPercent}</Label>
-                </Table.Cell>
-              </Table.Row>
-            )
-          })
-        }
-      </Table.Body>
-      <Table.Footer>
-      <Table.Row>
-        <Table.HeaderCell link='true'>More Games</Table.HeaderCell>
-        <Table.HeaderCell></Table.HeaderCell>
-        <Table.HeaderCell />
-      </Table.Row>
-      </Table.Footer>
-      </Table>
+            this.state.modalOpen &&
+
+            <Form onSubmit={this.handleSubmit}>
+              <Label>Name: </Label>
+              <Input name="name" value={this.state.name} onChange={this.handleChange}/> <br/>
+
+              <Label>Creator: </Label>
+              <Input name="creator" value={this.state.creator} onChange={this.handleChange}/>
+
+              <Label>Price: </Label>
+              <Input name="price" value={this.state.price} onChange={this.handleChange}/>
+
+              <Button>Submit</Button>
+
+            </Form>
+          }
       </div>
     )
   }
 }
-
-export default GameList
